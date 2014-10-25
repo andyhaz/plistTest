2c2e5a4c64924cbe69af98ebbc62b2213e495c92
andy <andyhaz1970@gmail.com>
2014-10-24 21:26:11 -0700

    Initial Commit

---
+++ plistTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  plistTest
+//
+//  Created by andy hazlett on 10/24/14.
+//  Copyright (c) 2014 andy hazlett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ plistTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  plistTest
+//
+//  Created by andy hazlett on 10/24/14.
+//  Copyright (c) 2014 andy hazlett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
